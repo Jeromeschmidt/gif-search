460dcf48dbb2431aeb510708ef56b2a27e64b5be
jeromeschmidt <schmidt.jerome@yahoo.com>
2019-09-18 17:10:38 -0700

    cleaned up app.py

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     #display random gifs on page before search
-    #r = requests.get("https://api.tenor.com/v1/trending?key=%s&limit=%s" % (params['key'], params['limit']))
-
-    # TODO: Extract the query term from url using request.args.get()
+    # Extract the query term from url using request.args.get()
     query = request.args.get('name')
-    # TODO: Make 'params' dictionary containing:
+    # Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
@@ -21,15 +19,15 @@ def index():
       'key': "5CFEKEV8TNLP",
       'limit': 10
     }
-    # TODO: Make an API call to Tenor using the 'requests' library. For
+    #Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
     try:
         r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params['q'], params['key'], params['limit']))
 
-    # TODO: Use the '.json()' function to get the JSON of the returned response
+    # Use the '.json()' function to get the JSON of the returned response
     # object
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
+    # Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
 
         result_list = list()
@@ -44,7 +42,7 @@ def index():
         return render_template("index.html", result_list=result_list, are_results=are_results)
     except:
         return render_template("index.html")
-    # TODO: Render the 'index.html' template, passing the list of gifs as a
+    # Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
 
 @app.route('/trending')
@@ -54,33 +52,23 @@ def trending():
       'key': "5CFEKEV8TNLP",
       'limit': 10
     }
-    # TODO: Make an API call to Tenor using the 'requests' library. For
+    # Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
     try:
         r = requests.get("https://api.tenor.com/v1/trending?key=%s&limit=%s" % (params['key'], params['limit']))
 
-    # TODO: Use the '.json()' function to get the JSON of the returned response
+    # Use the '.json()' function to get the JSON of the returned response
     # object
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
+    # Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-    # result_url = list()
-    # result_id = list()
-    # result_itemurl = list()
-    # result_dict = dict()
         result_list = list()
         are_results = False
 
         result_json = json.loads(r.content)
         are_results = True
         for i in range(params['limit']):
-            # result_dict[i]['id'] = result_id[i]
-            # result_dict[i]['itemurl'] = result_itemurl[i]
-            # result_dict[i]['url'] = result_url[i]
             result_list.append({"url": result_json['results'][i]['id'], "id": result_json['results'][i]['id'], "itemurl":result_json['results'][i]['itemurl']})
-            # result_url.append(result_json['results'][i]['media'][0]['mediumgif']['url'])
-            # result_id.append(result_json['results'][i]['id'])
-            # result_itemurl.append(result_json['results'][i]['itemurl'])
         return render_template("index.html", result_list=result_list, are_results=are_results)
     except:
         return render_template("index.html")
@@ -94,33 +82,23 @@ def random():
       'key': "5CFEKEV8TNLP",
       'limit': 10
     }
-    # TODO: Make an API call to Tenor using the 'requests' library. For
+    # Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
     try:
         r = requests.get("https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s" % (params['q'], params['key'], params['limit']))
 
-    # TODO: Use the '.json()' function to get the JSON of the returned response
+    # Use the '.json()' function to get the JSON of the returned response
     # object
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-    # result_url = list()
-    # result_id = list()
-    # result_itemurl = list()
-    # result_dict = dict()
         result_list = list()
         are_results = False
 
         result_json = json.loads(r.content)
         are_results = True
         for i in range(params['limit']):
-            # result_dict[i]['id'] = result_id[i]
-            # result_dict[i]['itemurl'] = result_itemurl[i]
-            # result_dict[i]['url'] = result_url[i]
             result_list.append({"url": result_json['results'][i]['id'], "id": result_json['results'][i]['id'], "itemurl":result_json['results'][i]['itemurl']})
-            # result_url.append(result_json['results'][i]['media'][0]['mediumgif']['url'])
-            # result_id.append(result_json['results'][i]['id'])
-            # result_itemurl.append(result_json['results'][i]['itemurl'])
         return render_template("index.html", result_list=result_list, are_results=are_results)
     except:
         return render_template("index.html")
